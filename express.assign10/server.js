20babe339a9e2c517f4220c9e1273ad10d658e6d
Akanksha13-dev <84703175+Akanksha13-dev@users.noreply.github.com>
2021-08-19 13:34:14 +0530

    Update server.js

--- express.assign10/server.js
+++ express.assign10/server.js
@@ -7,13 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 var PORT = 9000;
-// define a route handler for the default home page
-//app.use('/',require('./index.html'))
-app.use(express.static(path.join(__dirname,'/start/static')));
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,'/start/static/index1.html'))
-});
 app.use('/server2', require('./app/server2.js'));
 // start the Express server
 app.listen(PORT, function () {
